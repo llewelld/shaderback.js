0ccd40e8350887e4251d0cc08b98543f8da258be
David Llewellyn-Jones <david@flypig.co.uk>
2015-03-15 15:30:13 +0000

    Added namespace, updated presentation and example shaders.

--- examples/presentation/js/shaderback.js
+++ examples/presentation/js/shaderback.js
@@ -1,200 +1,241 @@
-var canvas;
-var loaded = false;
-var gl;
-var shaderProgram;
-var squareVertexPositionBuffer;
-var squareVertexTextureCoordBuffer;
-var pixelScale = 2.0;
+/***************************************************************************
+ * Shaderback
+ * Shader-generated backgrounds for the web
+ * http://www.flypig.co.uk/shaderback
+ * 
+ * David Llewellyn-Jones <david@flypig.co.uk>
+ * Version 0.02
+ * 15th March 2015
+ * 
+ * Copyright (c) 2015 David Llewellyn-Jones
+ * Released under the MIT license
+ * 
+***************************************************************************/
 
-var vsCode = ""
-	+ "	attribute vec3 aVertexPosition;"
-	+ "	attribute vec2 aTextureCoord;"
-	+ ""
-	+ "	varying vec2 vTextureCoord;"
-	+ ""
-	+ "	void main(void) {"
-	+ "		vTextureCoord = aTextureCoord;"
-	+ "		gl_Position = vec4(aVertexPosition, 1.0);"
-	+ "	}"
-	+ "";
-var fsCode;
-var running = false;
+/***************************************************************************
+ * To use, just add the following uncommented code to your HTML header
+ * 
+ * 	<script type="text/javascript" src="shaderback.js"></script>
+ * 	<script type="text/javascript">
+ * 	window.onload = shaderback.loadURL("shader.txt"));
+ * 	</script>
+ * 
+ * Where shader.txt is the URL of your fragment shader
+ * 
+ * You can also use loaddiv and loadtext rather than loadURL if your shader
+ * code is contained inside a script element or text variable respectively.
+ * 
+***************************************************************************/
 
-function shaderback_loadURL(url) {
-	var client = new XMLHttpRequest();
-	client.open('GET', url);
-	client.overrideMimeType("text/plain; charset=x-user-defined");
-	client.onloadend = function() {
-		fsCode = client.responseText;
+shaderback = function() {
+
+	var canvas;
+	var gl;
+	var shaderProgram;
+	var squareVertexPositionBuffer;
+	var squareVertexTextureCoordBuffer;
+	var pixelScale = 2.0;
+	var running = false;
+
+	var vsCode = ""
+		+ "	precision mediump float;"
+		+ ""
+		+ "	attribute vec3 aVertexPosition;"
+		+ "	attribute vec2 aTextureCoord;"
+		+ ""
+		+ "	varying vec2 vTextureCoord;"
+		+ ""
+		+ "	void main(void) {"
+		+ "		vTextureCoord = aTextureCoord;"
+		+ "		gl_Position = vec4(aVertexPosition, 1.0);"
+		+ "	}"
+		+ "";
+	var fsCode;
+
+	function loadURL(url) {
+		if (url) {
+			var client = new XMLHttpRequest();
+			client.open('GET', url);
+			client.overrideMimeType("text/plain; charset=x-user-defined");
+			client.onloadend = function() {
+				fsCode = client.responseText;
+				start();
+			}
+			client.send();
+		}
+	}
+
+	function loaddiv(id) {
+		fsCode = getShaderScript(gl, id);
 		start();
 	}
-	client.send();
-}
 
-function shaderback_loaddiv(id) {
-	fsCode = getShaderScript(gl, id);
-	start();
-}
-
-function shaderback_loadtext(text) {
-	fsCode = text;
-	start();
-}
-
-function ready() {
-	var div = document.createElement('div');
-	div.innerHTML = '<canvas class="shaderback" id="shaderback" width="500px" height="500px" style="width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: -1;"></canvas>';
-	var elements = div.childNodes[0];
-	document.getElementsByTagName('body')[0].appendChild(elements);
-	canvas = document.getElementById("shaderback");
-	window.onresize = resize;
-};
-
-function resize () {
-	var width = canvas.clientWidth / pixelScale;
-	var height = canvas.clientHeight / pixelScale;
-	if (canvas.width != width || canvas.height != height) {
-		canvas.width = width;
-		canvas.height = height;
-		canvas.style.height = window.innerHeight + "px";
+	function loadtext(text) {
+		fsCode = text;
+		start();
 	}
-}
 
-function start () {
-	if (!running) {
-		running = true;
-		ready();
-		resize();
-		initGL(canvas);
-		initShaders();
-		initBuffers();
-		drawScene();
-	}
-	else {
-		initShaders();
-	}
-}
-
-window.requestAnimFrame = (function() {
-return window.requestAnimationFrame ||
-	window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame ||
-	window.oRequestAnimationFrame ||
-	window.msRequestAnimationFrame ||
-	function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-		window.setTimeout(callback, 1000/60);
+	function ready() {
+		var div = document.createElement('div');
+		div.innerHTML = '<canvas class="shaderback" id="shaderback" width="500px" height="500px" style="width: 100%; height: 100%; position: fixed; top: 0px; left: 0px; z-index: -1;"></canvas>';
+		var elements = div.childNodes[0];
+		document.getElementsByTagName('body')[0].appendChild(elements);
+		canvas = document.getElementById("shaderback");
+		window.onresize = resize;
 	};
-})();
 
-function initGL(canvas) {
-	try {
-		gl = canvas.getContext("experimental-webgl");
-		gl.viewportWidth = canvas.width;
-		gl.viewportHeight = canvas.height;
-	} catch (e) {
-		// Fail silently
-	}
-}
-
-function getShaderScript(gl, id) {
-	var shaderScript = document.getElementById(id);
-	if (!shaderScript) {
-		return null;
-	}
-
-	var str = "";
-	var k = shaderScript.firstChild;
-	while (k) {
-		if (k.nodeType == 3) {
-			str += k.textContent;
+	function resize () {
+		var width = canvas.clientWidth / pixelScale;
+		var height = canvas.clientHeight / pixelScale;
+		if (canvas.width != width || canvas.height != height) {
+			canvas.width = width;
+			canvas.height = height;
+			canvas.style.height = window.innerHeight + "px";
 		}
-		k = k.nextSibling;
-	}
-	return str;
-}
-
-// Type should be either gl.FRAGMENT_SHADER or gl.VERTEX_SHADER
-function compileShader(str, type) {
-	var shader = gl.createShader(type);
-
-	gl.shaderSource(shader, str);
-	gl.compileShader(shader);
-
-	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-		alert(gl.getShaderInfoLog(shader));
-		return null;
 	}
 
-	return shader;
-}
-
-function initShaders() {
-	var fragmentShader = compileShader(fsCode, gl.FRAGMENT_SHADER);
-	var vertexShader = compileShader(vsCode, gl.VERTEX_SHADER);
-
-	shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram, vertexShader);
-	gl.attachShader(shaderProgram, fragmentShader);
-	gl.linkProgram(shaderProgram);
-
-	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-		alert("Could not initialise shaders");
+	function start () {
+		if (!running) {
+			running = true;
+			ready();
+			resize();
+			initGL(canvas);
+			initShaders();
+			initBuffers();
+			drawScene();
+		}
+		else {
+			initShaders();
+		}
 	}
 
-	gl.useProgram(shaderProgram);
+	window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame ||
+		window.mozRequestAnimationFrame ||
+		window.oRequestAnimationFrame ||
+		window.msRequestAnimationFrame ||
+		function(/* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+			window.setTimeout(callback, 1000/60);
+		};
+	})();
 
-	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+	function initGL(canvas) {
+		try {
+			gl = canvas.getContext("experimental-webgl");
+			gl.viewportWidth = canvas.width;
+			gl.viewportHeight = canvas.height;
+		} catch (e) {
+			// Fail silently
+		}
+	}
 
-	shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
-	gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+	function getShaderScript(gl, id) {
+		var shaderScript = document.getElementById(id);
+		if (!shaderScript) {
+			return null;
+		}
 
-	shaderProgram.timeUniform = gl.getUniformLocation(shaderProgram, "time");
-	shaderProgram.widthUniform = gl.getUniformLocation(shaderProgram, "width");
-	shaderProgram.heightUniform = gl.getUniformLocation(shaderProgram, "height");
-}
+		var str = "";
+		var k = shaderScript.firstChild;
+		while (k) {
+			if (k.nodeType == 3) {
+				str += k.textContent;
+			}
+			k = k.nextSibling;
+		}
+		return str;
+	}
 
-function initBuffers() {
-	squareVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-	vertices = [
-		1.0,  1.0,  0.0,
-		-1.0,  1.0,  0.0,
-		1.0, -1.0,  0.0,
-		-1.0, -1.0,  0.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	squareVertexPositionBuffer.itemSize = 3;
-	squareVertexPositionBuffer.numItems = 4;
+	// Type should be either gl.FRAGMENT_SHADER or gl.VERTEX_SHADER
+	function compileShader(str, type) {
+		var shader = gl.createShader(type);
 
-	squareVertexTextureCoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTextureCoordBuffer);
-	var textureCoords = [
-		1.0, 1.0,
-		0.0, 1.0,
-		1.0, 0.0,
-		0.0, 0.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-	squareVertexTextureCoordBuffer.itemSize = 2;
-	squareVertexTextureCoordBuffer.numItems = 4;
-}
+		gl.shaderSource(shader, str);
+		gl.compileShader(shader);
 
-function drawScene() {
-	var timeNow = new Date().valueOf();
-	requestAnimFrame(drawScene);
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+			alert(gl.getShaderInfoLog(shader));
+			return null;
+		}
 
-	gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
-	gl.uniform1f(shaderProgram.timeUniform, timeNow % 36000000);
-	gl.uniform1f(shaderProgram.widthUniform, canvas.clientWidth);
-	gl.uniform1f(shaderProgram.heightUniform, canvas.clientHeight);
+		return shader;
+	}
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	function initShaders() {
+		var fragmentShader = compileShader(fsCode, gl.FRAGMENT_SHADER);
+		var vertexShader = compileShader(vsCode, gl.VERTEX_SHADER);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTextureCoordBuffer);
-	gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, squareVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+		shaderProgram = gl.createProgram();
+		gl.attachShader(shaderProgram, vertexShader);
+		gl.attachShader(shaderProgram, fragmentShader);
+		gl.linkProgram(shaderProgram);
 
-	gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
-}
+		if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+			alert("Could not initialise shaders");
+		}
+
+		gl.useProgram(shaderProgram);
+
+		shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+		gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+		shaderProgram.textureCoordAttribute = gl.getAttribLocation(shaderProgram, "aTextureCoord");
+		gl.enableVertexAttribArray(shaderProgram.textureCoordAttribute);
+
+		shaderProgram.timeUniform = gl.getUniformLocation(shaderProgram, "time");
+		shaderProgram.widthUniform = gl.getUniformLocation(shaderProgram, "width");
+		shaderProgram.heightUniform = gl.getUniformLocation(shaderProgram, "height");
+	}
+
+	function initBuffers() {
+		squareVertexPositionBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+		vertices = [
+			1.0,  1.0,  0.0,
+			-1.0,  1.0,  0.0,
+			1.0, -1.0,  0.0,
+			-1.0, -1.0,  0.0
+		];
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+		squareVertexPositionBuffer.itemSize = 3;
+		squareVertexPositionBuffer.numItems = 4;
+
+		squareVertexTextureCoordBuffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTextureCoordBuffer);
+		var textureCoords = [
+			1.0, 1.0,
+			0.0, 1.0,
+			1.0, 0.0,
+			0.0, 0.0
+		];
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+		squareVertexTextureCoordBuffer.itemSize = 2;
+		squareVertexTextureCoordBuffer.numItems = 4;
+	}
+
+	function drawScene() {
+		var timeNow = new Date().valueOf();
+		requestAnimFrame(drawScene);
+
+		gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+		gl.uniform1f(shaderProgram.timeUniform, timeNow % 36000000);
+		gl.uniform1f(shaderProgram.widthUniform, canvas.clientWidth);
+		gl.uniform1f(shaderProgram.heightUniform, canvas.clientHeight);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
+		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+		gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexTextureCoordBuffer);
+		gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, squareVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+		gl.drawArrays(gl.TRIANGLE_STRIP, 0, squareVertexPositionBuffer.numItems);
+	}
+
+	return{
+		loadURL:loadURL,
+		loaddiv:loaddiv,
+		loadtext:loadtext
+	}
+}();
 
